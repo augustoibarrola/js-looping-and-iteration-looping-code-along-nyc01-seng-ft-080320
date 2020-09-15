df2c32e03fe54f9043c0a264b998783549edf74f
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-09-15 12:57:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+// for ([initialization]); [condition]; [iteration]) {
+//     [loop body]
+// }
+// initialization: typically where the counter is set (i = 0)
+// [condition]: if true, loop continues; if false, loop stops
+// [iteration]: statement at the end of every iteration (typically tics the counter up or down)
+// [loop body]: code that runs on each loop
+
+function writeCards(names, event) {
+    let namedGreetings = []
+    for ( let i = 0; i < names.length; i++ ) {
+        namedGreetings.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+    }
+    return namedGreetings
+  }
+  
+  function countDown(number) {
+      while (number > 0) {
+          console.log(number); 
+          number--
+      }
+      console.log(number)
+  }
+
